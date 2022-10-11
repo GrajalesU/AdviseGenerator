c1799816a4fe487d5bc6c592d1980564aa00713d
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2022-10-11 12:00:55 -0500

    Add functionality of Advice component

--- src/components/Advice/index.tsx
+++ src/components/Advice/index.tsx
@@ -1,14 +1,59 @@
 import './styles.css';
 
+import { useEffect, useState } from 'react';
+
 import diceSVG from '../../assets/icon-dice.svg';
+import Loader from '../Loader';
 
 const Advice = () => {
+  const [advice, setAdvice] = useState({
+    id: '',
+    description: '',
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://api.adviceslip.com/advice')
+      .then((res) => res.json())
+      .then(({ slip }) => {
+        setAdvice({
+          id: slip.id,
+          description: slip.advice,
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  const handleAdvice = () => {
+    setLoading(true);
+    fetch('https://api.adviceslip.com/advice')
+      .then((res) => res.json())
+      .then(({ slip }) => {
+        setAdvice({
+          id: slip.id,
+          description: slip.advice,
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   return (
     <div className="adviceCard">
-      <h1 className="adviceTitle">ADVICE # 117</h1>
-      <p className="adviceDescription">{`"It is easy to sit up and take notice, what's difficult is getting up and taking action."`}</p>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <h1 className="adviceTitle">ADVICE # {advice.id}</h1>
+          <p className="adviceDescription">{`"${advice.description}"`}</p>
+        </>
+      )}
       <div className="adviceDivider" />
-      <button className="adviceDice">
+      <button className="adviceDice" onClick={handleAdvice}>
         <img src={diceSVG} alt="Dice" />
       </button>
     </div>
